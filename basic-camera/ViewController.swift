31828a1b9ce6ca81c716dd225604966d2668f9c1
Yash Gupta <ygupta@microsoft.com>
2024-02-28 09:44:29 +0530

    Initial Commit

---
+++ basic-camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basic-camera
+//
+//  Created by Yash Gupta on 28/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ basic-cameraUITests/basic_cameraUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  basic_cameraUITestsLaunchTests.swift
+//  basic-cameraUITests
+//
+//  Created by Yash Gupta on 28/02/24.
+//
+
+import XCTest
+
+final class basic_cameraUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
